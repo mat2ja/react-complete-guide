5e2018fe67305f6a451dc1ef75f65120d3d34c77
Matija Osrečki <matija.osrecki@gmail.com>
2021-12-26 18:04:34 +0100

    Filter expenses by year assignment

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -9,14 +9,14 @@ const App = () => {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2019, 7, 14),
     },
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2020, 2, 28),
     },
     {
       id: 'e4',
@@ -27,8 +27,14 @@ const App = () => {
     {
       id: 'e5',
       title: 'Crypotag Zeus',
-      amount: 2756,
+      amount: 150,
       date: new Date(2021, 11, 26),
+    },
+    {
+      id: 'e6',
+      title: 'Crypotag Thor',
+      amount: 350,
+      date: new Date(2022, 2, 10),
     },
   ]);
 
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,13 +1,34 @@
-import ExpenseItem from './ExpenseItem.js';
+import React, { useState } from 'react';
+
 import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem.js';
+import ExpensesFilter from './ExpensesFilter.js';
 import './Expenses.css';
 
 const Expenses = ({ items }) => {
-  const expenseItems = items.map((expense) => (
-    <ExpenseItem expense={expense} key={expense.id} />
-  ));
+  const [filteredYear, setFilteredYear] = useState('');
 
-  return <Card className="expenses">{expenseItems}</Card>;
+  const filterChangeHandler = (selectedYear) => {
+    selectedYear = parseInt(selectedYear);
+    setFilteredYear(isNaN(selectedYear) ? '' : selectedYear);
+  };
+
+  const filterByYear = ({ date }) =>
+    filteredYear ? filteredYear === date.getFullYear() : true;
+
+  const expenseItems = items
+    .filter(filterByYear)
+    .map((expense) => <ExpenseItem expense={expense} key={expense.id} />);
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selectedYear={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expenseItems}
+    </Card>
+  );
 };
 
 export default Expenses;
