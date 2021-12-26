e4fc721672b0b41007ed76271425f3395cfe8e6b
Matija Osrečki <matija.osrecki@gmail.com>
2021-12-26 14:17:07 +0100

    Introduce useStateg

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,28 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const Expenseitem = ({ expense }) => {
+  const [title, setTitle] = useState(expense.title);
+
+  const clickHandler = (e) => {
+    setTitle('new title');
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date} />
       <div className="expense-item__description">
-        <h2>{expense.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">
           ${expense.amount.toLocaleString()}
         </div>
       </div>
+      <button onClick={clickHandler} onKeyDown={clickHandler}>
+        Change title
+      </button>
     </Card>
   );
 };
