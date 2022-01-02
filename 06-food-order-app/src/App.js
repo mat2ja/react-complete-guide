7d24f58373f59ea34a8bc33a62d282ea9a3d4cd4
Matija Osrečki <matija.osrecki@gmail.com>
2022-01-02 16:22:54 +0100

    Create header

---
+++ 06-food-order-app/src/App.js
@@ -0,0 +1,11 @@
+import Header from './components/Layout/Header.js';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ 06-food-order-app/src/components/UI/Button.js
@@ -0,0 +1,13 @@
+import classes from './Button.module.css'
+
+const Button = (props) => {
+  
+  
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Button
