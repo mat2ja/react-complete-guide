47e3277c0db509f47b283f010db42c402ebfb39b
Matija Osrečki <matija.osrecki@gmail.com>
2022-03-10 22:35:11 +0100

    Another useCallback example

--- 07-behind-the-scenes/src/App.js
+++ 07-behind-the-scenes/src/App.js
@@ -1,18 +1,26 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput.js';
 import Button from './components/UI/Button/Button.js';
 
 const App = () => {
   const [showP, setShowP] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  const toggleParaHandler = useCallback(() => setShowP((prev) => !prev), []);
+  const toggleParaHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowP((prev) => !prev);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => setAllowToggle(true);
 
   console.log('app running 🏃');
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <Button onClick={allowToggleHandler}>Allow toggle ✅</Button>
       <Button onClick={toggleParaHandler}>Toggle paragraph 🙉</Button>
       <DemoOutput show={showP} />
     </div>
