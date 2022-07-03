a789cc71b6a35e126edfbdf700b52adc8d05ccea
Matija Osrečki <matija.osrecki@gmail.com>
2022-07-03 12:19:20 +0200

    Fetch via async await

--- 08-sending-http-requests/src/App.js
+++ 08-sending-http-requests/src/App.js
@@ -7,18 +7,16 @@ import MoviesList from './components/MoviesList';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films/')
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedMovies = data.results.map((movie) => ({
-          id: movie.episode_id,
-          title: movie.title,
-          releaseDate: movie.release_date,
-          openingText: movie.opening_crawl,
-        }));
-        setMovies(transformedMovies);
-      });
+  const fetchMoviesHandler = async () => {
+    const url = 'https://swapi.dev/api/films/';
+    const movies = await fetch(url).then((res) => res.json());
+    const transformedMovies = movies.results.map((movie) => ({
+      id: movie.episode_id,
+      title: movie.title,
+      releaseDate: movie.release_date,
+      openingText: movie.opening_crawl,
+    }));
+    setMovies(transformedMovies);
   };
 
   useEffect(() => {
